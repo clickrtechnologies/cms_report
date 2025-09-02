c7095d82b6bb65c28aac2554506df253ddba61de
Rahul-090 <rs1692992@gmail.com>
2025-09-02 12:07:39 +0530

    apiurl for prod

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   // apiUrl: 'http://localhost:8084/' // trailing slash is good
 
-  apiUrl:'163.223.186.219:33060/8084/'
+   apiUrl: 'http://163.223.186.219:8084/' // backend server public IP + API port
 };
